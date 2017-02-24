bd6b488512f746a05e086f9aa61b55a719e225a5
Uneeck <urbantomasz87@gmail.com>
2017-02-24 14:38:47 +0100

    initial

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,9 @@ var value = (a*a) + (2 * a * b) - (b*b);
 console.log(value);
 if (value>0){
 	console.log('Wynik jest dodatni');
-} else {
+} else if (value<0) {
 	console.log('Wynik jest ujemny');
+} else {
+	console.log('Wynik jest równy zero');
 }
 
